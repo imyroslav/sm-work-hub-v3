98538dc562dd835ff131b07912b940a3a3333b2d
imyroslav <imyroslav@ukr.net>
2024-09-29 13:24:31 +0700

    image-routes-updated

--- src/gallery.js
+++ src/gallery.js
@@ -4,65 +4,65 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 const images = [
 {
 preview:
-'./images/preview-object-1.jpg',
+'../images/preview-object-1.jpg',
 original:
-'./images/original-object-1.jpg',
+'../images/original-object-1.jpg',
 description: 'Object-1',
 },
 {
 preview:
-'./images/preview-object-2.jpg',
+'../images/preview-object-2.jpg',
 original:
-'./images/original-object-2.jpg',
+'../images/original-object-2.jpg',
 description: 'Object-2',
 },
 {
 preview:
-'./images/preview-object-3.jpg',
+'../images/preview-object-3.jpg',
 original:
-'./images/original-object-3.jpg',
+'../images/original-object-3.jpg',
 description: 'Object-3',
 },
 {
 preview:
-'./images/preview-object-4.jpg',
+'../images/preview-object-4.jpg',
 original:
-'./images/original-object-4.jpg',
+'../images/original-object-4.jpg',
 description: 'Object-4',
 },
 {
 preview:
-'./images/preview-object-5.jpg',
+'../images/preview-object-5.jpg',
 original:
-'./images/original-object-5.jpg',
+'../images/original-object-5.jpg',
 description: 'Object-5',
 },
 {
 preview:
-'./images/preview-object-6.jpg',
+'../images/preview-object-6.jpg',
 original:
-'./images/original-object-6.jpg',
+'../images/original-object-6.jpg',
 description: 'Object-6',
 },
 {
 preview:
-'./images/preview-object-7.jpg',
+'../images/preview-object-7.jpg',
 original:
-'./images/original-object-7.jpg',
+'../images/original-object-7.jpg',
 description: 'Object-7',
 },
 {
 preview:
-'./images/preview-object-8.jpg',
+'../images/preview-object-8.jpg',
 original:
-'./images/original-object-8.jpg',
+'../images/original-object-8.jpg',
 description: 'Object-8',
 },
 {
 preview:
-'./images/preview-object-9.jpg',
+'../images/preview-object-9.jpg',
 original:
-'./images/original-object-9.jpg',
+'../images/original-object-9.jpg',
 description: 'Object-9',
 },
 ];
